c9e5af54695b8b8f4f152d004ed95a3d8117d4fa
celia <ccccelia777@gmail.com>
2019-11-26 22:03:36 -0800

    font and video

---
+++ css/showmore.js
@@ -0,0 +1,14 @@
+/*show more*/
+// <![CDATA[
+function ShowHide(divId)
+{
+if(document.getElementById(divId).style.display == 'none')
+{
+document.getElementById(divId).style.display='block';
+}
+else
+{
+document.getElementById(divId).style.display = 'none';
+}
+}
+// ]]>
